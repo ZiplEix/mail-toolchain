e1e9cb8ea4fa55dce3254d87057e2b21f31e3cba
ZiplEix <ZiplEix@users.noreply.github.com>
2025-07-03 15:58:04 +0900

    Ajoute la gestion des chemins de certificat et de clé via des variables d'environnement
    Adds support for certificate paths via environment variables
    
    Replaces hardcoded certificate and key paths with environment variables (CERT_PATH and KEY_PATH) to improve configurability.
    
    Includes new private key and certificate files in the repository for testing purposes. Ensures the application panics if the required environment variables are not set.

--- smtp-server/main.go
+++ smtp-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ZiplEix/mail-toolchain/smtp-server/db"
 	"github.com/ZiplEix/mail-toolchain/smtp-server/server"
@@ -16,7 +17,14 @@ func init() {
 		panic(fmt.Sprintf("Failed to initialize database: %v", err))
 	}
 
-	err = server.LoadTLSConfig("../cert.pem", "../key.pem")
+	certPath := os.Getenv("CERT_PATH")
+	keyPath := os.Getenv("KEY_PATH")
+
+	if certPath == "" || keyPath == "" {
+		panic("CERT_PATH or KEY_PATH environment variable is not set")
+	}
+
+	err = server.LoadTLSConfig(certPath, keyPath)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to load TLS configuration: %v", err))
 	}
